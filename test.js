69c72539c29bd5003c519de6b27e9f1b5a9b891e
Andrés Torres <125112555+andres-stg@users.noreply.github.com>
2024-03-02 02:42:58 +0000

    se hizo un borrador de tests para las 3 funciones de cambio que solicita el ejercicio

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+//euro to dollars
+
 test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js');
@@ -12,3 +14,17 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
+
+//dollar to yens
+test ("One dollar should be 150.10 yen", function () {
+    const { fromDollarToYen } = require('./app.js');
+    const expected = 150.10 * 2;
+    expect(fromDollarToYen(150.10)).toBe(300.2);
+})
+
+//yen to pounds
+test ("One yen should be 0.0053 pound", function () {
+    const { fromYenToPound } = require('./app.js');
+    const expected = 0.0053 * 100;
+    expect(fromYenToPound(0.0053)).toBe(0.53)
+})
